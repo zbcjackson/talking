e74687bb14f1b19eae557c8d819a490d9dbc6a67
Jackson Zhang <zbcjackson@gmail.com>
2016-02-27 13:23:58 +0800

    Clear text after sending; Enter to send

--- client/src/app/components/say.js
+++ client/src/app/components/say.js
@@ -2,14 +2,24 @@ import React from 'react'
 import {TextField, RaisedButton} from 'material-ui'
 
 class Say extends React.Component{
+  state={content: ''};
   submit(){
     var content = this.refs.content.getValue();
     this.props.onSubmit(content);
+    this.setState({content: ''});
+  }
+  onChange(e){
+    this.setState({content: e.target.value})
+  }
+  onKeyDown(e){
+    if (e.which === 13){
+      this.submit();
+    }
   }
   render(){
     return (
       <div>
-        <TextField ref='content' />
+        <TextField ref='content' value={this.state.content} onChange={(e) => this.onChange(e)} onKeyDown={(e) => this.onKeyDown(e)}/>
         <RaisedButton label='发送' primary={true} onTouchTap={() => this.submit()} />
       </div>
     );
